a371da46d11a6380e312f19cd59bce7448ea9ce8
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-03 07:33:39 +0300

    add on change event handler to al the three input fields

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,24 @@ function App() {
   const [year, setYear] = useState(0);
 
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value);
-    setDay(value);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, fieldType: string) => {
+    const value = parseInt(event.target.value);
     
+    if (fieldType === 'day') {
+      setDay(value);
+    } else if (fieldType === 'month') {
+      setMonth(value);
+    } else {
+      setYear(value)
+    }
+
   }
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // console.log(`Form submitted with day value: ${day}`);
+    console.log(`Form submitted with day value: ${day}`);
+    console.log(`Form submitted with month value: ${month}`);
+    console.log(`Form submitted with year value: ${year}`);
   }
 
   return (
@@ -27,17 +36,25 @@ function App() {
             <input 
               type="number" 
               id="day" 
-              onChange={handleInputChange} 
+              onChange={(event) => handleInputChange(event, 'day')} 
               // value={day ?? ''} 
             />
           </div>
           <div className="month input">
             <label htmlFor="month">MONTH</label>
-            <input type="number" id="month"/>
+            <input 
+              type="number" 
+              id="month"
+              onChange={(event) => handleInputChange(event, 'month')}
+              />
           </div>
           <div className="year input">
             <label htmlFor="year">YEAR</label>
-            <input type="number" id="year" />
+            <input 
+              type="number" 
+              id="year"
+              onChange={(event) => handleInputChange(event, 'year')}
+              />
           </div>
         </div>
         <hr />
