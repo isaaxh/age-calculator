d8798a9d816b3340b12584904e6bd1d4a0354bf9
Isaac Hussain <isaaclearns58@gmail.com>
2023-04-06 16:27:25 +0300

    feat: add leapyear in calculation of daysInFeb

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
   const [errorYear, setErrorYear] =  useState<string>();
   const [errorMonth, setErrorMonth] =  useState<string>();
   const [errorDay, setErrorDay] =  useState<string>();
+  const [isError, setIsError] = useState<boolean>();
 
 
   const calcAge = (startDateStr: string, endDateStr: string) => {
@@ -57,17 +58,51 @@ function App() {
 
   }
 
+  const isLeapYear= (year: number) => {
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+      console.log(`${year} is leap year`);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setErrorYear('')
+    setIsError(false);
+    setErrorYear('');
+    setErrorMonth('');
+    setErrorDay('');
     const currentDate = new Date(); 
     const currentDateStr = currentDate.toISOString().slice(0, 10);
-
     const currentYear = currentDate.getFullYear();
+    const isCurrentLeapYear = isLeapYear(currentYear);
 
     if (inputYear > currentYear) {
       setErrorYear('Must be in the past');
+      setIsError(true);
       return;
+    }
+
+    if (inputMonth < 1 || inputMonth > 12) {
+      setErrorMonth('Must be a valid month');
+      setIsError(true);
+      return;
+    }
+
+    if (inputDay < 0 || inputDay > 31) {
+      setErrorDay('Must be a valid day')
+      setIsError(true)
+      return;
+    }
+
+    if (inputMonth === 2) {
+      const daysInFebruary = isCurrentLeapYear ? 29 : 28;
+      if (inputDay > daysInFebruary) {
+        setErrorDay('Must be a valid day');
+        setIsError(true);
+        return;
+      }
     }
 
     const age = calcAge(`${inputYear}-${inputMonth}-${inputDay}`, currentDateStr);
@@ -80,11 +115,13 @@ function App() {
       <form className="card" onSubmit={handleFormSubmit}>
         <div className="input-section">
           <div className="day input">
-            <label htmlFor="day">DAY</label>
+            <label className="error" htmlFor="day">DAY</label>
             <input 
               type="number" 
+              className='error-field'
               id="day" 
               onChange={(event) => handleInputChange(event, 'day')} 
+              placeholder='DD'
             />
             <p className="error">{errorDay}</p>
           </div>
@@ -94,6 +131,7 @@ function App() {
               type="number" 
               id="month"
               onChange={(event) => handleInputChange(event, 'month')}
+              placeholder='MM'
               />
               <p className="error">{errorMonth}</p>
           </div>
@@ -103,6 +141,7 @@ function App() {
               type="number" 
               id="year"
               onChange={(event) => handleInputChange(event, 'year')}
+              placeholder='YYYY'
               />
               <p className="error">{errorYear}</p>
           </div>
@@ -111,9 +150,9 @@ function App() {
         <button>Calculate</button>
         <hr />
         <div className="output-section">
-          <div className="year-output">{user?.years} years</div>
-          <div className="month-output">{user?.months} months</div>
-          <div className="day-output">{user?.days} days</div>
+          <div className="output"><span className='year output-value'>{user?.years}</span> years</div>
+          <div className="output"><span className='month output-value'>{user?.months}</span> months</div>
+          <div className="output"><span className='day output-value'>{user?.days}</span> days</div>
         </div>
       </form>
     </div>
